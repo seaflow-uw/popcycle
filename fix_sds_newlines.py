26e494abce7450c16a3b017f0e978219f15cb42c
Francois Ribalet <ribalet@uw.edu>
2014-05-16 16:08:27 -0700

    from Dan, during the last cruise on the Kilo-Moana (July 2013)

---
+++ fix_sds_newlines.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from collections import defaultdict
+import os
+import sys
+import string
+
+DEBUG = False
+if DEBUG:
+    counts = defaultdict(int)
+
+for line in sys.stdin:
+    # Remove any trailing newline characters
+    while len(line) > 0 and (line[-1] == '\r' or line[-1] == '\n'):
+        line = line[:-1]
+    # Split the line into fields based on tabs
+    line = line.split('\t')
+
+    # debugging: keep track of how many of each line length we saw
+    if DEBUG:
+        counts[len(line)] += 1
+
+    # First line of the file, just print it
+    if line[0] == 'FILE':
+        sys.stdout.write("%s" % '\t'.join(line))
+    # Any line that starts with 'sds', terminate the previous line and print it
+    elif line[0].startswith('sds'):
+        sys.stdout.write("%s%s" % (os.linesep, '\t'.join(line)))
+    # Any other line, just print it
+    else:
+        sys.stdout.write("%s" % '\t'.join(line))
+
+# Terminate the lsat line
+sys.stdout.write(os.linesep)
+
+# For debugging, print the line length counts to stderr
+if DEBUG:
+    print >> sys.stderr, counts
