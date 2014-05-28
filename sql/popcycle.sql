2a410c93538b4c53490b7878fae6b52397e32b71
Daniel Halperin <dhalperi@cs.washington.edu>
2014-05-28 12:25:31 -0700

    sql: add popcycle.sql, which creates the evt, opp, vct, and sds tables

---
+++ sql/popcycle.sql
@@ -0,0 +1,58 @@
+CREATE TABLE evt (
+  -- First three columns are the EVT, OPP, VCT composite key
+  cruise TEXT NOT NULL,
+  file TEXT NOT NULL,  -- in old files, File+Day. in new files, Timestamp.
+  particle INTEGER NOT NULL,
+  -- Next we have the measurements
+  time INTEGER NOT NULL,
+  pulse_width INTEGER NOT NULL,
+  D1 INTEGER NOT NULL,
+  D2 INTEGER NOT NULL,
+  fsc_small INTEGER NOT NULL,
+  fsc_perp INTEGER NOT NULL,
+  fsc_big INTEGER NOT NULL,
+  pe INTEGER NOT NULL,
+  chl_small INTEGER NOT NULL,
+  chl_big INTEGER NOT NULL,
+  PRIMARY KEY (cruise, file, particle)
+);
+
+CREATE TABLE opp (
+  -- First three columns are the EVT, OPP, VCT composite key
+  cruise TEXT NOT NULL,
+  file TEXT NOT NULL,  -- in old files, File+Day. in new files, Timestamp.
+  particle INTEGER NOT NULL,
+  -- Next we have the measurements
+  time INTEGER NOT NULL,
+  pulse_width INTEGER NOT NULL,
+  D1 INTEGER NOT NULL,
+  D2 INTEGER NOT NULL,
+  fsc_small INTEGER NOT NULL,
+  fsc_perp INTEGER NOT NULL,
+  fsc_big INTEGER NOT NULL,
+  pe INTEGER NOT NULL,
+  chl_small INTEGER NOT NULL,
+  chl_big INTEGER NOT NULL,
+  PRIMARY KEY (cruise, file, particle)
+);
+
+CREATE TABLE vct (
+  -- First three columns are the EVT, OPP, VCT, SDS composite key
+  cruise TEXT NOT NULL,
+  file TEXT NOT NULL,  -- in old files, File+Day. in new files, Timestamp.
+  particle INTEGER NOT NULL,
+  -- Next we have the measurements
+  pop TEXT NOT NULL,
+  method TEXT NOT NULL,
+  PRIMARY KEY (cruise, file, particle)
+);
+
+CREATE TABLE sds (
+  -- First two columns are the SDS composite key
+  cruise TEXT NOT NULL,
+  file TEXT NOT NULL,  -- in old files, File+Day. in new files, Timestamp.
+  -- Next we have the measurements
+  lat REAL,
+  lon REAL,
+  PRIMARY KEY (cruise, file)
+);
