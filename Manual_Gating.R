9ae80362021f93e9f4175b4c4585f6b1dbbd44a0
Francois Ribalet <ribalet@uw.edu>
2014-05-28 10:10:13 -0700

    New SeaFLow data structure

--- Manual_Gating.R
+++ Manual_Gating.R
@@ -1,6 +1,7 @@
 library(flowCore)
 library(splancs) 
 library(flowPhyto)
+cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
 
 opp.path <- system.file("extdata","seaflow_cruise","2011_001", "2.evt.opp", package="flowPhyto")
 opp <- readSeaflow(opp.path, transform=F)
@@ -18,20 +19,20 @@ par(mfrow=c(1,1), pty="s")
 para <- c("chl_small","pe")
 
 x <- subset(opp, pop==0)
-plot(x[,para], pch=16, cex=0.4, col = densCols(x[,para], colramp = .rainbow.cols), main="Gating BEADS")
+plot(x[,para], pch=16, cex=0.4, col = densCols(x[,para], colramp = cols), main="Gating BEADS")
 
 print("Gating Beads")
-poly.beads <- getpoly(quiet=TRUE)
+poly.beads <- getpoly(quiet=TRUE); colnames(poly.beads) <- para
 beads <- subset(x,inout(x[,para],poly=poly.beads, bound=TRUE, quiet=TRUE))
 
 
 ### SYN
 para <- c("fsc_small","pe")
-plot(x[,para], pch=16, cex=0.4, col = densCols(x[,para], colramp = .rainbow.cols), main="Gating Synecho")
+plot(x[,para], pch=16, cex=0.4, col = densCols(x[,para], colramp = cols), main="Gating Synecho")
 points(beads[,para],col=2)
 
 print("Gating Synecho")
-poly.syn <- getpoly(quiet=TRUE)
+poly.syn <- getpoly(quiet=TRUE); colnames(poly.syn) <- para
 syn <- subset(x,inout(x[,para],poly=poly.syn, bound=TRUE, quiet=TRUE))
 
 
