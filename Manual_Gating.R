b6a2ce3101f28ae2f78cdaabbcbea25f9af6aaa9
Francois Ribalet <ribalet@uw.edu>
2014-06-03 16:08:07 -0700

    SetGateParams + Gating function

--- Manual_Gating.R
+++ Manual_Gating.R
@@ -1,44 +1,82 @@
-library(flowCore)
-library(splancs) 
-library(flowPhyto)
+setGateParams <- function(opp, popname, dim.x, dim.y, override=TRUE){
+
+require(splancs)
+
 cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
 
+
+popname <- as.character(popname)
+dim.x <- as.character(dim.x)
+dim.y <- as.character(dim.y)
+
+
+para <- c(dim.x, dim.y)
+
+par(mfrow=c(1,1), pty="s", cex=1)
+plot(opp[,para], pch=16, cex=0.4, col = densCols(log10(opp[,para]), colramp = cols), main=paste("Set Gate for:",popname), log='xy') #plot 2D cytogram
+poly <- getpoly(quiet=TRUE) # Draw Gate
+colnames(poly) <- para
+
+write.csv(poly, paste(gating.param.location, "/",popname,".csv",sep=""), quote=FALSE, row.names=FALSE)
+
+
+time <- format(Sys.time(),format="%FT%H-%M-%S+0000", tz="GMT")
+write.csv(poly, paste(gating.param.location_archived, "/",popname,".",time,".csv",sep=""), quote=FALSE, row.names=FALSE) # TODO(hyrkas): add archive_folder for Params in Globals
+
+return(poly)
+
+}
+
+
+
+
+
+
+
+gating <- function(opp){
+
+opp$pop <- "unknown"
+
+list.params <- list.files(path=gating.param.location, pattern= ".csv", full.names=TRUE)
+
+	for(p in list.params){
+
+		pop <- basename(sub(".csv","",p)) # name of the population
+		poly <- read.csv(p) # Get parameters of the gate for this population
+		para <- colnames(poly)
+		df <- subset(opp, pop=="unknown")[,para]
+
+		colnames(poly) <- colnames(df) <- c("x","y") # to stop stupid Warnings from inout()
+		vct <- subset(df, inout(df,poly=poly, bound=TRUE, quiet=TRUE)) # subset particles based on Gate
+		opp[row.names(vct),"pop"] <- pop
+		
+	}
+
+	return(opp$pop)
+
+}
+
+
+
+
+
+
+
+### testing
+
 opp.path <- system.file("extdata","seaflow_cruise","2011_001", "2.evt.opp", package="flowPhyto")
-opp <- readSeaflow(opp.path, transform=F)
-opp$pop <- 0
+opp <- readSeaflow(opp.path, transform=T)
+
+pop1 <- setGateParams(opp, popname="beads",dim.x="chl_small", dim.y="pe")
+pop2 <- setGateParams(opp, popname="synecho",dim.x="fsc_small", dim.y="chl_small")
+
+vct <- gating(opp)
 
 
 
-##############
-### GATING ###
-##############
-par(mfrow=c(1,1), pty="s")
-
-### BEADS
-
-para <- c("chl_small","pe")
-
-x <- subset(opp, pop==0)
-plot(x[,para], pch=16, cex=0.4, col = densCols(x[,para], colramp = cols), main="Gating BEADS")
-
-print("Gating Beads")
-poly.beads <- getpoly(quiet=TRUE); colnames(poly.beads) <- para
-beads <- subset(x,inout(x[,para],poly=poly.beads, bound=TRUE, quiet=TRUE))
 
 
-### SYN
-para <- c("fsc_small","pe")
-plot(x[,para], pch=16, cex=0.4, col = densCols(x[,para], colramp = cols), main="Gating Synecho")
-points(beads[,para],col=2)
-
-print("Gating Synecho")
-poly.syn <- getpoly(quiet=TRUE); colnames(poly.syn) <- para
-syn <- subset(x,inout(x[,para],poly=poly.syn, bound=TRUE, quiet=TRUE))
 
 
-##################
-### ASSIGNMENT ###
-##################
 
-opp[row.names(beads),'pop'] <- "beads"
-opp[row.names(syn),'pop'] <- "syn"
+
