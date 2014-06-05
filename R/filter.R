f9b13ff4781bf0b17ccc993655490feaf7e07e2a
Francois Ribalet <ribalet@uw.edu>
2014-06-04 17:46:35 -0700

    notch optimization + visualization

--- R/filter.R
+++ R/filter.R
@@ -1,13 +1,13 @@
-filter.notch <- function(events, width, notch) {
+filter.notch <- function(evt, width, notch) {
 
   # TODO(hyrkas): if data is not log transformed, leave it be?
-  if(any(max(events[,-c(1,2)]) > 10^3.5)){
+  if(any(max(evt[,-c(1,2)]) > 10^3.5)){
     stop(paste("ERROR, data are not LOG-transform"))
   }
   
   ##### FILTRING OPP #####
-  detected <- subset(events, D1 > 1 & D2 > 1) # filtering particles not detected by D1 or D2
-  unsaturated <- subset(detected, D1 < max(events[,"D1"]) & D2 < max(events[,"D1"])) # filtering particles with saturated signals on D1 or D2
+  detected <- subset(evt, D1 > 1 & D2 > 1) # filtering particles not detected by D1 or D2
+  unsaturated <- subset(detected, D1 < max(evt[,"D1"]) & D2 < max(evt[,"D1"])) # filtering particles with saturated signals on D1 or D2
   unsaturated[,-c(1,2)] <- (log10(unsaturated[,-c(1,2)])/3.5)*2^16 ## linearize the LOG transformed data
   
   origin.unsaturated <- subset(unsaturated, D1 > 10000 & D2 > 10000) # Exclude potenital electrical noise from calculation.	
@@ -18,8 +18,35 @@ filter.notch <- function(events, width, notch) {
   
   aligned <- subset(unsaturated, D2 > (D1 - width * 10^4) & D2 < (D1 + width*10^4)) # filtering aligned particles (D1 = D2)
   focused <- subset(aligned, D2/fsc_small > (D1/fsc_small - notch) & D2/fsc_small < (D1/fsc_small + notch))# filtering focused particles (D1/fsc_small = D2/fsc_small)
-  filtered <- subset(focused, D1/fsc_small < notch | D2/fsc_small < notch) # filtering focused particles (D/fsc_small < notch)
-  filtered[,-c(1,2)] <-  10^((filtered[,-c(1,2)]/2^16)*3.5)
+  opp <- subset(focused, D1/fsc_small < notch | D2/fsc_small < notch) # filtering focused particles (D/fsc_small < notch)
+  opp[,-c(1,2)] <-  10^((opp[,-c(1,2)]/2^16)*3.5)
   
-  return(filtered)
+  return(opp)
+}
+
+
+best.filter.notch <- function(evt, notch=seq(0.4, 1.4, by=0.1),width =1, do.plot=TRUE){
+
+  DF <- NULL
+  
+  for(n in notch){
+    opp <- filter.notch(evt, notch=n, width=width)
+    fsc.max <- round(max(opp[,"fsc_small"]))
+    para <- data.frame(cbind(notch=n, fsc.max))
+    DF <- rbind(DF, para)
+    }
+
+  best.notch.id <- min(which(DF$fsc.max == max(DF$fsc.max)))
+  best.notch <- DF$notch[best.notch.id]
+
+  if(do.plot){
+  par(mfrow=c(2,1),cex=1)
+  par(pty='m')
+  plot(DF[,c('notch', 'fsc.max')], main=paste("Best notch=",best.notch)); points(best.notch, DF$fsc.max[best.notch.id], col=2, pch=16)
+  opp <- filter.notch(evt, notch=best.notch, width=width)
+  plot.cytogram(opp,"fsc_small","chl_small", vct=FALSE); mtext(paste("OPP with notch=",best.notch),3,line=1)
+  }
+
+return(best.notch)
+
 }
--- R/visualization.R
+++ R/visualization.R
@@ -1,35 +1,104 @@
-plotCytogram <- function(opp, para.x, para.y, vct = TRUE){		
+plot.cytogram <- function(opp, para.x, para.y, vct = FALSE){		
 	
 
-	breaks <- 10^((0:24)*3.5/24)	# log - spaced grid
 	cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
 
-	hist.x <- hist(opp[,para.x], breaks=breaks, plot=FALSE)
-	hist.y <- hist(opp[,para.y], breaks=breaks, plot=FALSE)
+	par(pty='s')
 
-	
-	def.par <- par(no.readonly = TRUE) # save default, for resetting...
-	nf <- layout(matrix(c(2,0,1,3),2,2,byrow=TRUE), c(3,1,3,1,3), c(1,3,1,3,1,3), TRUE)
-
-	par(mar=c(6,6,1,1),pty='s')
 	if(vct==FALSE) plot(opp[,c(para.x, para.y)], pch=16, cex=0.4, col = densCols(log10(opp[,c(para.x, para.y)]), colramp = cols), log='xy') #plot 2D cytogram
 	
 	if(vct==TRUE){
+
 		plot(opp[,c(para.x, para.y)], pch=16, cex=0.4, col = as.numeric(as.factor(opp$pop)), log='xy') #plot 2D cytogram
 		legend('topleft',legend=(unique(opp$pop)), col=unique(as.numeric(as.factor(opp$pop))), pch=16,pt.cex=0.4,bty='n')
-	}
-	par(mar=c(0,6,1,1), pty='m')
-	barplot(hist.x$counts, axes=FALSE, space=0, col=NA)
-	par(mar=c(6,0,1,1),pty='m')
-	barplot(hist.y$counts, axes=FALSE, space=0, horiz=TRUE, col=NA)
-
-   
-par(def.par)   	  
+	}	  
 
 }
 
+
+
+plot.filter.cytogram <- function(evt, width=0.5, notch=1){
+  cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
+
+
+  notch <- as.numeric(notch)
+  width<- as.numeric(width)
+   
+   if(any(max(evt[,-c(1,2)]) > 10^3.5)){
+    stop(paste("ERROR, data are not LOG-transform"))
+   }
+   
+  detected <- subset(evt, D1 > 1 & D2 > 1) # filtering particles not detected by D1 or D2
+  unsaturated <- subset(detected, D1 < max(evt[,"D1"]) & D2 < max(evt[,"D1"])) # filtering particles with saturated signals on D1 or D2
+  unsaturated[,-c(1,2)] <- (log10(unsaturated[,-c(1,2)])/3.5)*2^16 ## linearize the LOG transformed data
+  
+  origin.unsaturated <- subset(unsaturated, D1 > 10000 & D2 > 10000) # Exclude potenital electrical noise from calculation.	
+  origin <- median(origin.unsaturated$D2) - median(origin.unsaturated$D1) 	# Difference of sensitivity between D2 and D1.	 
+  # Correction for the difference of sensitivity between D1 and D2
+  if(origin > 0)  unsaturated$D2 <- 	unsaturated$D2 - origin
+  if(origin < 0)  unsaturated$D1 <- 	unsaturated$D1 + origin	
+  
+  aligned <- subset(unsaturated, D2 > (D1 - width * 10^4) & D2 < (D1 + width*10^4)) # filtering aligned particles (D1 = D2)
+  focused <- subset(aligned, D2/fsc_small > (D1/fsc_small - notch) & D2/fsc_small < (D1/fsc_small + notch))# filtering focused particles (D1/fsc_small = D2/fsc_small)
+  opp <- subset(focused, D1/fsc_small < notch | D2/fsc_small < notch) # filtering focused particles (D/fsc_small < notch)
+  opp[,-c(1,2)] <-  10^((opp[,-c(1,2)]/2^16)*3.5)  
+
+  ########################
+  
+	percent.opp <- round(100*nrow(opp)/nrow(evt),1)
+	
+	par(mfrow=c(2,2),pty='s')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+	
+	events. <- evt[1:round(nrow(opp)),]
+    
+    plot.cytogram(events., "D1", "D2",vct=FALSE)
+	 mtext("Filtering Aligned Particles", side=3, line=1, font=2)
+	 mtext(paste("only", percent.opp,"% display"), side=3, cex=0.7,col='red')
+	 abline(v=max(evt[,"D1"]), h=max(evt[,"D2"]), col='red',lwd=2)
+	 abline(v=1, h=1, col='red',lwd=2)
+		 par(new=T)
+	 plot(1,1, xlim=c(0,2^16),ylim=c(0,2^16), bty='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, pch=NA)
+	 abline(b=1, a=origin + width*10^4, col='red',lwd=2)
+	 abline(b=1, a=origin -width*10^4, col='red',lwd=2)
+
+	mtext(paste("Sensitivity Difference D2 =", round(origin,0)),outer=T,side=3, line=-1.5,font=2)
+	mtext(paste("Width=", width),outer=T,side=3, line=-3,font=2)
+	mtext(paste("Notch=", notch),outer=T,side=3, line=-4,font=2)
+
+	aligned. <- subset(aligned, aligned$D1/aligned$fsc_small<2 & aligned$D2/aligned$fsc_small<2)[1:round(nrow(opp)),]
+    aligned.$para1 <- aligned.$D1/aligned.$fsc_small
+    aligned.$para2 <- aligned.$D2/aligned.$fsc_small
+    plot.cytogram(aligned., 'para1',  'para2')
+     mtext("Filtering Focused Particles", side=3, line=1, font=2)
+	 mtext(paste("only", percent.opp,"% display"), side=3, cex=0.7,col='red')
+     abline(v=notch, h=notch, col='red',lwd=2)
+	 abline(b=1, a=notch, col='red', lwd=2)
+	 abline(b=1, a=-notch, col='red', lwd=2)
+		
+   plot.cytogram(opp, "fsc_small", "pe")
+   mtext("Optimally Positioned Particles", side=3, line=1, font=2)
+ 	plot.cytogram(opp, "fsc_small","chl_small")
+   mtext("Optimally Positioned Particles", side=3, line=1, font=2)
+ 
+  }
+
+
+
+
+
+
+
+
+
+
 # testing
+evt.path <- system.file("extdata","seaflow_cruise","2011_001", "3.evt", package="flowPhyto")
+evt <- readSeaflow(opp.path, transform=T)
+
+plot.filter.cytogram(evt, width=0.5, notch=1)
+
+notch <- find.filter.notch(evt,notch=seq(0.4, 1.4, by=0.1),width =1, do.plot=TRUE)
+
 #opp.path <- system.file("extdata","seaflow_cruise","2011_001", "2.evt.opp", package="flowPhyto")
 #opp <- readSeaflow(opp.path, transform=T)
-
-#plotCytogram(opp, "fsc_small", "chl_small",classification=TRUE)
+#plot.cytogram(opp, "fsc_small", "chl_small",classification=TRUE)
