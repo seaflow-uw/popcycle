e3717d3e7d4dbffeb52ab412f32eab461356e25b
Jeremy Hyrkas <hyrkas@ninetales.cs.washington.edu>
2014-06-06 16:13:33 -0700

    Added code to insert to database. Next up: testing

--- fix_sfl.py
+++ fix_sfl.py
@@ -1,8 +1,10 @@
+import sqlite3
+
 DELIM = '\t'
 
 CRUISE = 'CRUISE'                          # str
 FILE = 'FILE'                              # str
-date = 'DATE'                              # str
+DATE = 'DATE'                              # str
 FILE_DURATION = 'FILE DURATION'            # float
 LAT = 'LAT'                                # str because it needs the '+'/'-'? Format: Decimal Degrees (DDD)
 LON = 'LON'                                # str because it needs the '+'/'-'? Format: Decimal Degrees (DDD)
@@ -16,7 +18,7 @@ EVENT_RATE  = 'EVENT RATE'                 # int
 
 FLOATS = [FILE_DURATION, SALINITY, OCEAN_TEMP, BULK_RED, STREAM_PRESSURE, CONDUCTIVITY]
 INTS = [EVENT_RATE]
-STRS = [FILE, LAT, LON, PAR]
+STRS = [FILE, LAT, LON, PAR, DATE]
 
 DB_COLUMNS = ['CRUISE', 'FILE', 'DATE', 'FILE_DURATION', 'LAT', 'LON',
               'CONDUCTIVITY', 'SALINITY', 'OCEAN_TEMP', 'PAR', 'BULK_RED',
@@ -63,16 +65,36 @@ def fix_and_insert_sfl(data, header, dbpath, cruise=cruise_id):
                 except:
                     dbcolumn_to_fixed_data[h] = None
         # else, do nothing
-        
+
+    # add cruise and data 
     dbcolumn_to_fixed_data[CRUISE] = cruise
+    try :
+      iso_split = dbcolumn_to_fixed_data[FILE].split('T')
+      print iso_split
+      iso_split[1] = iso_split[1].replace('-', ':')
+      iso_split[1] = iso_split[1][:-2] + ':' + iso_split[1][-2:]
+      dbcolumn_to_fixed_data[DATE] = 'T'.join(iso_split)
+    except:
+      dbcolumn_to_fixed_data[DATE] = None
             
     # any fields that weren't passed in should be None
     for c in DB_COLUMNS:
         if not c in dbcolumn_to_fixed_data:
             dbcolumn_to_fixed_data[c] = None
 
-    # TODO: insert dbcolumn_to_fixed_data to db. 
-    return dbcolumn_to_fixed_data
+    # populate list
+    db_tuple = []
+    for c in DB_COLUMNS :
+      db_tuple.append(dbcolumn_to_fixed_data[c])
+
+    # insert into sqlite
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    c.execute("insert into sfl values (?,?,?,?,?,?,?,?,?,?,?,?,?)", tuple(db_tuple))
+    conn.commit()
+    conn.close()
+
+    #return db_tuple
 
 def fix_lat_or_lon(l):
     return None
