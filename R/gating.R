d7f344552bde71009a30402d2c0a6f76a60c2bd1
Francois Ribalet <ribalet@uw.edu>
2014-06-06 16:28:06 -0700

    Remove warnings

--- R/gating.R
+++ R/gating.R
@@ -24,16 +24,18 @@ setGateParams <- function(opp, popname, para.x, para.y, override=TRUE){
   write.csv(poly, paste0(param.gate.location, "/",popname, ".csv"), quote=FALSE, row.names=FALSE)
 
 # load the list of gate parameters
-  params <- try(load(paste0(param.gate.location,"/params.RData")),silent=T)
+  params <- list.files(param.gate.location,"params.RData")
 # if list already exists, modify the list, if not, save the list
-  if(class(params)==("try-error")){
+  if(length(params)==0){
      poly.log <- poly.l
      save(poly.log, file=paste0(param.gate.location,"/params.RData"))
    }else{
+    # if gate paramters for the same population already exist, overwrite, otherwise, add the new gate parameters to the end of the array.
+    load(paste0(param.gate.location,"/params.RData"))
     id <- which(names(poly.log) == popname)
-          if(length(id)>0){ # if gate paramters for the same population already exist, overwrite. 
-          poly.log[[id]] <- poly
-        }else{poly.log <- c(poly.log, poly.l)} # otherwise, add the new gate parameters to the end of the array.
+          if(length(id)==0){poly.log <- c(poly.log, poly.l)
+            }else{poly.log[[id]] <- poly} 
+   
    save(poly.log, file=paste0(param.gate.location,"/params.RData"))
   }
 
@@ -46,11 +48,11 @@ Gating <- function(opp,gate_path=param.gate.location){
 
   opp$pop <- "unknown"
   
-  params <- try(load(paste0(param.gate.location,"/params.RData")),silent=T)
-  if(class(params)==("try-error")){
+  params <- list.files(param.gate.location,"params.RData")
+  if(length(params)==0){
     print("No gate parameters found!")
     stop
-   }
+   }else{load(paste0(param.gate.location,"/params.RData"))}
 
 	for(i in 1:length(poly.log)){
 		pop <- names(poly.log[i]) # name of the population
--- R/visualization.R
+++ R/visualization.R
@@ -1,4 +1,4 @@
-plot.cytogram <- function(opp, para.x, para.y){		
+plot.cytogram <- function(opp, para.x = 'fsc_small', para.y = 'chl_small'){		
 
 	cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
 	par(pty='s')
@@ -7,11 +7,12 @@ plot.cytogram <- function(opp, para.x, para.y){
 }
 
 ## OPP merged with VCT
-plot.vct.cytogram <- function(opp,para.x, para.y){		
+plot.vct.cytogram <- function(opp,para.x = 'fsc_small', para.y = 'chl_small'){		
 		
 		if(!is.null(opp$pop)){
 			par(pty='s')
-			plot(opp[,c(para.x, para.y)], pch=16, cex=0.6, col = as.numeric(as.factor(opp$pop)), log='xy') #plot 2D cytogram
+      ## TODO[francois] Order OPP by frequency (most abundant pop plotted first, least abundant pop plotted last)
+      plot(opp[,c(para.x, para.y)], pch=16, cex=0.6, col = as.numeric(as.factor(opp$pop)), log='xy') #plot 2D cytogram
 			legend('topleft',legend=(unique(opp$pop)), col=unique(as.numeric(as.factor(opp$pop))), pch=16,pt.cex=0.6,bty='n')
 		}else{
 			print("No Gating parameters yet!")
@@ -19,6 +20,19 @@ plot.vct.cytogram <- function(opp,para.x, para.y){
 			mtext(paste("No Gating parameters yet!"),3,line=-1,font=2)
 		}
 }
+
+
+
+plot.vct.cytogram.by.file <- function(file_name, para.x = 'fsc_small', para.y = 'chl_small'){
+  
+  vct <- get_vct_by_file(file_name)
+  opp <- get_opp_by_file(file_name)
+  opp$pop <- vct
+  plot.vct.cytogram(opp, para.x = para.x, para.y = para.y)
+
+}
+
+
 
 
 plot.filter.cytogram <- function(evt, width=1, notch=1){
@@ -40,7 +54,7 @@ plot.filter.cytogram <- function(evt, width=1, notch=1){
   ### FILTERING OPP ###
   #####################
     # filtering aligned particles (D1 = D2), with Correction for the difference of sensitivity between D1 and D2
-	if(origin > 1) aligned <- subset(evt, D2 * origin < (D1 * width) & D2 * origin > (D1 / width))
+    if(origin > 1) aligned <- subset(evt, D2 * origin < (D1 * width) & D2 * origin > (D1 / width))
   	if(origin < 1) aligned <- subset(evt, D2 < ((D1/origin) * width) & D2 > ((D1/origin)/ width))
   	
   	# filtering focused particles (D/fsc_small < notch)
@@ -56,34 +70,34 @@ plot.filter.cytogram <- function(evt, width=1, notch=1){
 	
 	if(nrow(evt) > 1000){display <- 1000}else{display <- nrow(evt)}
 
-par(mfrow=c(2,2),pty='s')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-plot.cytogram(evt[1:display,], "D1", "D2")
-	mtext("Alignment", side=3, line=1, font=2)
-	lines(c(1,m.D1),c(1,m.D1)*width/origin, lwd=2, col=2)
-	lines(c(1,m.D1),c(1,m.D1)/(width*origin), lwd=2, col=2)
-	mtext(paste("D1/D2 =", round(origin,2)),outer=T,side=3, line=-1.5,font=2)
-	mtext(paste("Width=", width-1),outer=T,side=3, line=-3,font=2)
-	mtext(paste("Notch=", notch),outer=T,side=3, line=-4,font=2)
-	mtext(paste("OPP =", percent.opp,"% EVT"), outer=T,side=1, line=-1.5,font=2,col=2)
+  par(mfrow=c(2,2),pty='s')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+  plot.cytogram(evt[1:display,], "D1", "D2")
+  	mtext("Alignment", side=3, line=1, font=2)
+  	lines(c(1,m.D1),c(1,m.D1)*width/origin, lwd=2, col=2)
+  	lines(c(1,m.D1),c(1,m.D1)/(width*origin), lwd=2, col=2)
+  	mtext(paste("D1/D2 =", round(origin,2)),outer=T,side=3, line=-1.5,font=2)
+  	mtext(paste("Width=", width-1),outer=T,side=3, line=-3,font=2)
+  	mtext(paste("Notch=", notch),outer=T,side=3, line=-4,font=2)
+  	mtext(paste("OPP =", percent.opp,"% EVT"), outer=T,side=1, line=-1.5,font=2,col=2)
 
-	aligned <- subset(aligned[1:display,], D1/fsc_small<2 & D2/fsc_small<2)
-    if(origin > 1){ 
-    	aligned$D1.fsc_small <- aligned$D1/aligned$fsc_small
-    	aligned$D2.fsc_small <- origin*aligned$D2/aligned$fsc_small
-    }
-    if(origin < 1){
-    	aligned$D1.fsc_small <- (aligned$D1/origin)/aligned$fsc_small
-    	aligned$D2.fsc_small <- aligned$D2/aligned$fsc_small
-    }
-plot(aligned[,c("D1.fsc_small", "D2.fsc_small")], pch=16, cex=0.6, col = densCols(aligned[,c("D1.fsc_small", "D2.fsc_small")], colramp = cols), xlim=c(0,2), ylim=c(0,2)) 
-     mtext("Focus", side=3, line=1, font=2)
-     abline(v=notch, h=notch, col=2,lwd=2)
-	 # abline(b=1, a=notch, col='red', lwd=2)
-	 # abline(b=1, a=-notch, col='red', lwd=2)
-	
-plot.cytogram(opp, "fsc_small", "pe")
-   	mtext("OPP", side=3, line=1, font=2)
-plot.cytogram(opp, "fsc_small","chl_small")
-   	mtext("OPP", side=3, line=1, font=2)
+  	aligned <- subset(aligned[1:display,], D1/fsc_small<2 & D2/fsc_small<2)
+      if(origin > 1){ 
+      	aligned$D1.fsc_small <- aligned$D1/aligned$fsc_small
+      	aligned$D2.fsc_small <- origin*aligned$D2/aligned$fsc_small
+      }
+      if(origin < 1){
+      	aligned$D1.fsc_small <- (aligned$D1/origin)/aligned$fsc_small
+      	aligned$D2.fsc_small <- aligned$D2/aligned$fsc_small
+      }
+  plot(aligned[,c("D1.fsc_small", "D2.fsc_small")], pch=16, cex=0.6, col = densCols(aligned[,c("D1.fsc_small", "D2.fsc_small")], colramp = cols), xlim=c(0,2), ylim=c(0,2)) 
+       mtext("Focus", side=3, line=1, font=2)
+       abline(v=notch, h=notch, col=2,lwd=2)
+  	 # abline(b=1, a=notch, col='red', lwd=2)
+  	 # abline(b=1, a=-notch, col='red', lwd=2)
+  	
+  plot.cytogram(opp, "fsc_small", "pe")
+     	mtext("OPP", side=3, line=1, font=2)
+  plot.cytogram(opp, "fsc_small","chl_small")
+     	mtext("OPP", side=3, line=1, font=2)
  
 }
