305a7b5ba79eb023e1ee1f35628bf0e783b79968
Francois Ribalet <ribalet@uw.edu>
2014-06-11 19:17:35 +0100

    try/catch in re-run code
    in case there's a problematic file

--- R/rerun_analyses.R
+++ R/rerun_analyses.R
@@ -6,28 +6,32 @@ rerun_filter <- function(start_day, start_timestamp, end_day, end_timestamp) {
   }
   
   for (i in 1:length(files)) {
-    evt_file = files[i]
-    file_name = basename(evt_file)
-    print(paste('Filtering', evt_file))
-    evt <- readSeaflow(paste(evt.location, evt_file, sep='/'))
+  	#if we get an error, move to next file
+    tryCatch({
+    	evt_file = files[i]
+	    file_name = basename(evt_file)
+   		print(paste('Filtering', evt_file))
+	    evt <- readSeaflow(paste(evt.location, evt_file, sep='/'))
     
-    opp <- filter_evt(evt, filter.notch, width = params$width, notch = params$notch)
-    # delete old opp entries if they exist so we keep cruise/file/particle distinct
-    .delete_opp_by_file(file_name)
-    # store opp
-    print('Uploading filtered particles to database')
-    upload_opp(opp_to_db_opp(opp, cruise.id, file_name))
+	    opp <- filter_evt(evt, filter.notch, width = params$width, notch = params$notch)
+	    # delete old opp entries if they exist so we keep cruise/file/particle distinct
+	    .delete_opp_by_file(file_name)
+	    # store opp
+	    print('Uploading filtered particles to database')
+	    upload_opp(opp_to_db_opp(opp, cruise.id, file_name))
     
-    if (length(list.files(path=param.gate.location, pattern= ".csv", full.names=TRUE)) > 0) {
-      print(paste('Classifying', evt_file))
-      vct <- classify_opp(opp, Gating, param.gate.location)
-      # delete old vct entries if they exist so we keep cruise/file/particle distinct
-      .delete_vct_by_file(file_name)
-      # store vct
-      print('Uploading labels to the database')
-      upload_vct(vct_to_db_vct(vct, cruise.id, file_name, 'Manual Gating'))
-      # TODO: insert statistics 
-    }
+	    if (length(list.files(path=param.gate.location, pattern= ".csv", full.names=TRUE)) > 0) {
+	      print(paste('Classifying', evt_file))
+	      vct <- classify_opp(opp, Gating, param.gate.location)
+	      # delete old vct entries if they exist so we keep cruise/file/particle distinct
+	      .delete_vct_by_file(file_name)
+	      # store vct
+	      print('Uploading labels to the database')
+	      upload_vct(vct_to_db_vct(vct, cruise.id, file_name, 'Manual Gating'))
+	      # TODO: insert statistics 
+	    }
+	}, error = print(paste("Encountered error with file", evt_file))
+	)
   }
 }
 
@@ -38,16 +42,19 @@ rerun_gating <- function(start_day, start_timestamp, end_day, end_timestamp) {
   }
   
   for (i in 1:length(files)) {
-    evt_file = files[i]
-    file_name = basename(evt_file)
-    opp <- get_opp_by_file(file_name)
-    print(paste('Classifying', evt_file))
-    vct <- classify_opp(opp, Gating, param.gate.location)
-    # delete old vct entries if they exist so we keep cruise/file/particle distinct
-    .delete_vct_by_file(file_name)
-    # store vct
-    print('Uploading labels to the database')
-    upload_vct(vct_to_db_vct(vct, cruise.id, file_name, 'Manual Gating'))
-    # TODO: insert statistics
+  	tryCatch({
+    	evt_file = files[i]
+    	file_name = basename(evt_file)
+    	opp <- get_opp_by_file(file_name)
+    	print(paste('Classifying', evt_file))
+    	vct <- classify_opp(opp, Gating, param.gate.location)
+    	# delete old vct entries if they exist so we keep cruise/file/particle distinct
+    	.delete_vct_by_file(file_name)
+    	# store vct
+    	print('Uploading labels to the database')
+    	upload_vct(vct_to_db_vct(vct, cruise.id, file_name, 'Manual Gating'))
+    	# TODO: insert statistics
+    }, error = print(paste("Encountered error with file", evt_file))
+    )
   }
 }
