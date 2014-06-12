33d0d816672dda490c5e39302009c0b2f6582ee2
Francois Ribalet <ribalet@uw.edu>
2014-06-12 16:39:07 -0700

    plot stats

--- R/visualization.R
+++ R/visualization.R
@@ -104,48 +104,55 @@ plot.filter.cytogram <- function(evt, width=1, notch=1){
 }
 
 
-# plot.map <- function(stat,popname,...){
-#   ## plot cell abundances of a population on a map
-#   require(maps, quietly=T)
-#   require(mapdata, quietly=T)
-#   require(plotrix, quietly=T)
+plot.map <- function(stat,popname,param,...){
+  ## plot cell abundances of a population on a map
+  require(maps, quietly=T)
+  require(mapdata, quietly=T)
+  require(plotrix, quietly=T)
 
-#   cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
+  cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
 
-#   map.type <- 'worldHires'
+  map.type <- 'worldHires'
   
-#     xlim <- range(stat$long, na.rm=T)        
-#     ylim <- range(stat$lat, na.rm=T)   
+    xlim <- range(stat$long, na.rm=T)        
+    ylim <- range(stat$lat, na.rm=T)   
     
-#     if(xlim[1] < 0 & xlim[2] > 0){
-#         neg.long <- subset(stat, long < 0)
-#       stat[row.names(neg.long), "long"] <- neg.long$long + 360
-#       xlim <- c(min(stat$long, na.rm=TRUE), max(stat$long, na.rm=TRUE))
-#       stat <- stat[-which(is.na(stat$long)),]
-#       stat$long[stat$long < 0] <- stat$long[stat$long < 0] + 360
-#       map.type <- 'world2Hires'
-#         }
+    if(xlim[1] < 0 & xlim[2] > 0){
+        neg.long <- subset(stat, long < 0)
+      stat[row.names(neg.long), "long"] <- neg.long$long + 360
+      xlim <- c(min(stat$long, na.rm=TRUE), max(stat$long, na.rm=TRUE))
+      stat <- stat[-which(is.na(stat$long)),]
+      stat$long[stat$long < 0] <- stat$long[stat$long < 0] + 360
+      map.type <- 'world2Hires'
+        }
   
-#   # plot the cruise track as gray line back-ground
-#   pop <- subset(stat, pop == popname)
-#   par(oma=c(1,2,1,3))
-#   plot(pop$long, pop$lat, xlim=xlim, ylim=ylim, asp=1, main=paste(popname),
-#             xlab="Longitude (deg W)",ylab="Latitude (deg N)",type='l',lwd=3,col='lightgrey',...)
-#   try(maps::map(map.type, fill=TRUE, col='lightgrey',add=TRUE))
-#   points(pop$long, pop$lat, pch=16, asp=1, col=cols(100)[cut(pop$conc,100)],...)
+  # plot the cruise track as gray line back-ground
+  pop <- subset(stat, pop == popname)
+  par(oma=c(1,1,1,2), mar=c(4,4,3,4))
+  plot(pop$long, pop$lat, xlim=xlim, ylim=ylim, asp=1, main=paste(popname),
+            xlab="Longitude (deg W)",ylab="Latitude (deg N)",type='l',lwd=3,col='lightgrey',...)
+  try(maps::map(map.type, fill=F, col='black',add=TRUE))
+  points(pop$long, pop$lat, pch=16, asp=1, col=cols(100)[cut(pop[,param],100)],...)
 
-#     ylim <- par('usr')[c(3,4)]
-#     xlim <- par('usr')[c(1,2)]
+    ylim <- par('usr')[c(3,4)]
+    xlim <- par('usr')[c(1,2)]
 
 
 
-#     color.legend(xlim[2], ylim[1], xlim[2] + 0.02*diff(xlim), ylim[2], 
-#       legend=pretty(pop$conc), rect.col=cols(100), gradient='y',align='rb',...)
-#   mtext(substitute(paste("Abundances (10"^{6},"cells L"^{-1},")")), side=4, line=3)  
+    color.legend(xlim[2], ylim[1], xlim[2] + 0.02*diff(xlim), ylim[2], 
+      legend=pretty(pop[,param]), rect.col=cols(100), gradient='y',align='rb',...)
+  mtext(paste(param), side=4, line=3)  
   
 
-# }
+}
+
+plot.time <- function(stat, popname,param, ...){
+
+stat$time <- as.POSIXct(stat$time)
+pop <- subset(stat, pop == popname)
+
+par(oma=c(1,1,1,1), mar=c(4,4,3,4))
+plot(pop$time, pop[,param], xlim="time", ylim=paste(param),...)
 
 
-
-# stat <- read.delim("/Volumes/seaflow/Tokyo_2/stats.tab")
+}
--- executable_scripts/cron_job.R
+++ executable_scripts/cron_job.R
@@ -8,20 +8,15 @@ evaluate.last.evt()
 
 
 
-########################
-### load latest file ###
-########################
+######################
+### PLOT CYTOGRAMS ###
+######################
 
 last.file <- get.latest.file()
 opp <- get.opp.by.file(last.file)
 vct <- get.vct.by.file(last.file)
 opp$pop <- vct
 
-
-
-######################
-### PLOT CYTOGRAMS ###
-######################
 png("~/cytogram.png",width=15,height=9,unit='in',res=300)
 par(mfrow=c(1,2),cex=1.4)
 plot.vct.cytogram(opp, "fsc_small","chl_small")
@@ -29,3 +24,32 @@ plot.vct.cytogram(opp, "fsc_small","pe")
 mtext(paste(last.file), side=3, line=-1,outer=T)
 dev.off()
 
+
+##################
+### PLOT STATS ###
+##################
+stat <- get.stat.table()  
+phyto <- unique(stat$pop)
+
+png("cell_conc_map.png",width=15, height=9, unit='in', res=300)
+par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
+for(i in phyto)	plot.map (stat, popname=i, param='abundance')
+dev.off()
+
+
+png("cell_conc_time.png",width=15, height=9, unit='in', res=300)
+par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
+for(i in phyto)	plot.time(stat, popname=i, param='abundance')
+dev.off()
+
+
+png("~/light_scatter.png",width=15,height=9,unit='in',res=300)
+par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
+for(i in phyto)	plot.time(stat, popname=i, param='fsc_small')
+dev.off()
+
+
+png("~/chl_small.png",width=15,height=9,unit='in',res=300)
+par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
+for(i in phyto)	plot.time(stat, popname=i, param='chl_small')
+dev.off()
