7b39938382e49a193898b98f0fcbf8621ea8a2cf
Francois Ribalet <ribalet@uw.edu>
2014-06-13 11:00:14 -0700

    try() in plot

--- executable_scripts/cron_job.R
+++ executable_scripts/cron_job.R
@@ -35,23 +35,23 @@ phyto <- unique(stat$pop)
 
 png("cell_conc_map.png",width=15, height=9, unit='in', res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
-for(i in phyto)	plot.map (stat, popname=i, param='abundance')
+for(i in phyto)	try(plot.map (stat, popname=i, param='abundance'),silent=TRUE)
 dev.off()
 
 
 png("cell_conc_time.png",width=15, height=9, unit='in', res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
-for(i in phyto)	plot.time(stat, popname=i, param='abundance')
+for(i in phyto)	try(plot.time(stat, popname=i, param='abundance'),silent=TRUE)
 dev.off()
 
 
 png("~/light_scatter.png",width=15,height=9,unit='in',res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
-for(i in phyto)	plot.time(stat, popname=i, param='fsc_small')
+for(i in phyto)	try(plot.time(stat, popname=i, param='fsc_small'),silent=TRUE)
 dev.off()
 
 
 png("~/chl_small.png",width=15,height=9,unit='in',res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
-for(i in phyto)	plot.time(stat, popname=i, param='chl_small')
+for(i in phyto)	try(plot.time(stat, popname=i, param='chl_small'),silent=TRUE)
 dev.off()
