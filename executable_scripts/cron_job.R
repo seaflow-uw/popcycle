a712ac3e770d89cd1d961b27fca9a653904e3fc1
Francois Ribalet <ribalet@uw.edu>
2014-06-14 15:25:28 +0100

    cex

--- executable_scripts/cron_job.R
+++ executable_scripts/cron_job.R
@@ -11,6 +11,7 @@ evaluate.last.evt()
 ######################
 ### PLOT CYTOGRAMS ###
 ######################
+cex=1.4
 
 last.file <- get.latest.file()
 opp <- get.opp.by.file(last.file)
@@ -18,7 +19,7 @@ vct <- get.vct.by.file(last.file)
 opp$pop <- vct
 
 png("~/cytogram.png",width=15,height=9,unit='in',res=300)
-par(mfrow=c(1,2),cex=1.4)
+par(mfrow=c(1,2),cex=cex)
 plot.vct.cytogram(opp, "fsc_small","chl_small")
 plot.vct.cytogram(opp, "fsc_small","pe")
 mtext(paste(last.file), side=3, line=-1,outer=T)
@@ -30,6 +31,7 @@ dev.off()
 ##################
 stat <- get.stat.table()  
 phyto <- unique(stat$pop)
+
 
 png("cell_conc_map.png",width=15, height=9, unit='in', res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
