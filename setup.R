45225b536ba613175f5973d09ea3df307854bc7b
hyrkas <jeremy.hyrkas@gmail.com>
2014-06-14 11:24:49 -0700

    install requirements if they're not already installed

--- setup.R
+++ setup.R
@@ -1,1 +1,9 @@
+if (!('RSQLite' %in% rownames(installed.packages()))) {
+  install.packages('RSQLite')
+}
+
+if (!('splancs' %in% rownames(installed.packages()))) {
+  install.packages('splancs')
+}
+
 install.packages('.', repos=NULL, type='source')
