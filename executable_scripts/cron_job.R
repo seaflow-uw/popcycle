b3f0a252f1d32f259b462b278845bb502cf4cef9
Francois Ribalet <ribalet@uw.edu>
2014-06-14 18:30:29 +0100

    change some plotting parameters

--- executable_scripts/cron_job.R
+++ executable_scripts/cron_job.R
@@ -22,7 +22,7 @@ png("~/cytogram.png",width=15,height=9,unit='in',res=300)
 par(mfrow=c(1,2),cex=cex)
 plot.vct.cytogram(opp, "fsc_small","chl_small")
 plot.vct.cytogram(opp, "fsc_small","pe")
-mtext(paste(last.file), side=3, line=-1,outer=T)
+mtext(paste(last.file), side=3, line=-3,outer=T,cex=cex)
 dev.off()
 
 
@@ -35,23 +35,23 @@ phyto <- unique(stat$pop)
 
 png("cell_conc_map.png",width=15, height=9, unit='in', res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
-for(i in phyto)	try(plot.map (stat, popname=i, param='abundance'),silent=TRUE)
+for(i in phyto)	try(plot.map (stat, popname=i, param='abundance'))
 dev.off()
 
 
 png("cell_conc_time.png",width=15, height=9, unit='in', res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
-for(i in phyto)	try(plot.time(stat, popname=i, param='abundance'),silent=TRUE)
+for(i in phyto)	try(plot.time(stat, popname=i, param='abundance'))
 dev.off()
 
 
 png("~/light_scatter.png",width=15,height=9,unit='in',res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
-for(i in phyto)	try(plot.time(stat, popname=i, param='fsc_small'),silent=TRUE)
+for(i in phyto)	try(plot.time(stat, popname=i, param='fsc_small'))
 dev.off()
 
 
 png("~/chl_small.png",width=15,height=9,unit='in',res=300)
 par(mfrow=c(ceiling(length(phyto)/2),2), cex=cex, mar=c(2,2,2,3), oma=c(1,1,3,0))
-for(i in phyto)	try(plot.time(stat, popname=i, param='chl_small'),silent=TRUE)
+for(i in phyto)	try(plot.time(stat, popname=i, param='chl_small'))
 dev.off()
