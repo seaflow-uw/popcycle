96063301e3b48ea2cd05babb4c4a73b3f3a9b120
Daniel Halperin <dhalperi@cs.washington.edu>
2014-06-27 23:35:29 -0700

    force error in bash script

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ mkdir ~/popcycle/logs
 mkdir ~/popcycle/logs/gates
 mkdir ~/popcycle/logs/filter
 
+sdfsdf
 # Create the initial database
 sqlite3 ~/popcycle/sqlite/popcycle.db < sql/popcycle.sql
 # Run the R setup scripts
