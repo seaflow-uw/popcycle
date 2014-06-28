fcff2db2cf1a54384d98e37e1e87db2db47ec226
Daniel Halperin <dhalperi@cs.washington.edu>
2014-06-28 00:11:12 -0700

    setup.R: fail if any package fails to install

--- setup.R
+++ setup.R
@@ -1,32 +1,24 @@
-quit_with_error <- function() {
-  # Quit R with error status
-  q(status = 1)
-}
-options(error = quit_with_error)
-
-if (!('RSQLite' %in% rownames(installed.packages()))) {
-  install.packages('RSQLite', repos='http://cran.us.r-project.org')
-}
-
-if (!('splancs' %in% rownames(installed.packages()))) {
-  install.packages('splancs', repos='http://cran.us.r-project.org')
-}
-
-if (!('plyr' %in% rownames(installed.packages()))) {
-  install.packages('plyr', repos='http://cran.us.r-project.org')
-}
-
-if (!('maps' %in% rownames(installed.packages()))) {
-  install.packages('maps', repos='http://cran.us.r-project.org')
+try_to_install <- function(pkg, repos, type) {
+  if (pkg %in% rownames(installed.packages())) {
+    return
+  }
+  if (missing(repos)) {
+    repos = 'http://cran.us.r-project.org'
+  }
+  if (missing(type)) {
+    type = NULL
+  }
+  install.packages(pkg, repos=repos, type=type)
+  if (!(pkg %in% rownames(installed.packages()))) {
+    # Quit R with error status
+    q(status = 1)
+  }
 }
 
-if (!('mapdata' %in% rownames(installed.packages()))) {
-  install.packages('mapdata', repos='http://cran.us.r-project.org')
-}
-
-if (!('plotrix' %in% rownames(installed.packages()))) {
-  install.packages('plotrix', repos='http://cran.us.r-project.org')
-}
-
-
-install.packages('.', repos=NULL, type='source')
+try_to_install('RSQLite')
+try_to_install('splancs')
+try_to_install('plyr')
+try_to_install('maps')
+try_to_install('mapdata')
+try_to_install('plotrix')
+try_to_install('.', repos=NULL, type='source')
