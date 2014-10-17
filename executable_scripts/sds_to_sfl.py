d7d377a945513c6ff3628d1c238dff9cc15c6f1c
Chris Berthiaume <chrisbee@uw.edu>
2014-10-16 17:34:34 -0700

    SDS to SFL conversion script now adds a 'FILE DURATION' field set to 180.  It also replaces periods in column headers with a single space for compatibility with tools designed for SFL files.  Lines for files already encountered are ignored.  Construct an ISO 8601 SFL 'DATE' field from SDS 'time' field.

--- executable_scripts/sds_to_sfl.py
+++ executable_scripts/sds_to_sfl.py
@@ -5,17 +5,31 @@ f2 = open(sys.argv[2], 'w')
 
 header = f.readline()
 elements = header.split('\t')
-elements[-2] = 'FILE'
-f2.write('\t'.join(elements[:-1]))
+elements[-2] = 'FILE'           # replace 'day' column with 'FILE'
+elements[-1] = 'FILE DURATION'  # replace 'file' column
+elements[-3] = 'DATE'           # replace 'time' column
+
+# Older SDS files had periods between words of a single header column
+# e.g. STREAM.PRESSURE
+# To make these column headers compatible with new SFL file processing
+# these periods should be converted to a single space
+elements = [x.replace('.', ' ') for x in elements]
+
+f2.write('\t'.join(elements))
 f2.write('\n')
 
+files_seen = set()
 for line in f :
   elements = line.split('\t')
+  # Build 'FILE' value from 'file' and 'day'
   elements[-2] = elements[-2] + '/' + elements[-1].strip() + '.evt'
-  f2.write('\t'.join(elements[:-1]))
-  f2.write('\n')
+  # FILE DURATION values fixed at 180 seconds
+  elements[-1] = '180'
+  # Construct a ISO 8601 date string from 'time' field of SDS file and
+  # replace 'time' field with this string
+  elements[-3] = elements[-3].replace(' ', 'T') + '+0000'
 
-# TODO : 
-# 1) REPLACE "." by " " in column names of the SDS.TAB
-# 2) ADD Column FILE DURATION and filled it with value 180
-# 3) REMOVE duplicate filename, if present.
+  if not (elements[-2] in files_seen):
+    files_seen.add(elements[-2])
+    f2.write('\t'.join(elements))
+    f2.write('\n')
