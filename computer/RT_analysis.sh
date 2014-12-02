5e1e2aae9b8b9801a60f18bbc116ec62a6730e17
Chris Berthiaume <chrisbee@uw.edu>
2014-12-02 13:30:59 -0800

    Fix RT_analysis.sh script paths.

--- computer/RT_analysis.sh
+++ computer/RT_analysis.sh
@@ -5,10 +5,10 @@ if shlock -f ${lckfile} -p $$
 then
 echo "cron job starting at $(date)"
 rm ~/cron_job.out
-Rscript ~/popcycle-master/popcycle/executable_scripts/copy_file.R >& ~/cron_job.out
-python ~/popcycle-master/popcycle/executable_scripts/fix_sfl_newlines.py >> ~/cron_job.out 2 >& 1
-python ~/popcycle-master/popcycle/executable_scripts/fix_sfl.py >> ~/cron_job.out 2 >& 1
-Rscript ~/popcycle-master/popcycle/executable_scripts/cron_job.R >> ~/cron_job.out 2 >& 1
+Rscript ~/popcycle-master/executable_scripts/copy_file.R >& ~/cron_job.out
+python ~/popcycle-master/executable_scripts/fix_sfl_newlines.py >> ~/cron_job.out 2 >& 1
+python ~/popcycle-master/executable_scripts/fix_sfl.py >> ~/cron_job.out 2 >& 1
+Rscript ~/popcycle-master/executable_scripts/cron_job.R >> ~/cron_job.out 2 >& 1
 rm ${lckfile}
 else
 echo "cron job did not run, lock ${lckfile} already held by $(cat ${lckfile})"
