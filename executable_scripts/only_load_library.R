010031b31370671447b1603209b837f5233a0c70
Chris Berthiaume <chrisbee@uw.edu>
2014-12-04 17:08:19 -0800

    Convenience scripts to reset realtime analysis data.

---
+++ executable_scripts/only_load_library.R
@@ -0,0 +1,4 @@
+# Only load the popcycle library
+# Useful if you want to recreate the default ~/popcycle/ directory if it has
+# been erased.
+library(popcycle)
---
+++ executable_scripts/reset_realtime.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Reset realtime analysis data.  This script should be run if you want to
+# completely restart a realtime analysis.
+# CAUTION: It will erase all current work in the project!
+
+# This script will erase
+#  ~/stat.csv
+#  ~/sfl.csv
+#  ~/RT_analysis.sh
+#  ~/cron_job.out
+#  ~/SeaFlow/datafiles/evt/*
+#  ~/popcycle/
+# 
+# This script will also recreate ~/popcycle/ with no filtering or gating
+# parameters and an popcycle sqlite3 database
+
+rm ~/stat.csv
+rm ~/sfl.csv
+rm ~/RT_analysis.sh
+rm ~/cron_job.out
+rm -rf ~/SeaFlow/datafiles/evt/*
+rm -rf ~/popcycle
+
+Rscript ~/popcycle-master/executable_scripts/only_load_library.R
