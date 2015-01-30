482edc719b0c8394a5b4b423c639a1d6a5acee98
Chris Berthiaume <chrisbee@uw.edu>
2015-01-30 12:43:03 -0800

    First attempt at handling new version of seaflow workflow where SFL files always have FLOW RATE, either calculated by instrument software or in sds_to_sfl.py based on instrument serial number.

---
+++ executable_scripts/import_sfl.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+"""
+Import SFl file into sqlite3 database.  Assumes FLOW RATE is a column in input
+SFL file.
+"""
+
+import sqlite3
+import os
+import glob
+import re
+import sys
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+DELIM = '\t'
+
+CRUISE = 'CRUISE'                          # str
+FILE = 'FILE'                              # str
+DATE = 'DATE'                              # str
+FILE_DURATION = 'FILE DURATION'            # float
+LAT = 'LAT'                                # float --> Format: Decimal Degrees (DDD)
+LON = 'LON'                                # float --> Format: Decimal Degrees (DDD)
+CONDUCTIVITY = 'CONDUCTIVITY'              # float
+SALINITY = 'SALINITY'                      # float
+OCEAN_TEMP = 'OCEAN TEMP'                  # float
+PAR = 'PAR'                                # float
+BULK_RED  = 'BULK RED'                     # float
+STREAM_PRESSURE  = 'STREAM PRESSURE'       # float
+FLOW_RATE = 'FLOW RATE'                    # float
+EVENT_RATE  = 'EVENT RATE'                 # int
+
+FLOATS = [FILE_DURATION, SALINITY, OCEAN_TEMP, BULK_RED, STREAM_PRESSURE, FLOW_RATE, CONDUCTIVITY, PAR, LAT, LON]
+INTS = [EVENT_RATE]
+STRS = [FILE, DATE]
+
+DB_COLUMNS = ['CRUISE', 'FILE', 'DATE', 'FILE_DURATION', 'LAT', 'LON',
+              'CONDUCTIVITY', 'SALINITY', 'OCEAN_TEMP', 'PAR', 'BULK_RED',
+              'STREAM_PRESSURE', 'FLOW_RATE','EVENT_RATE']
+
+
+# takes data and header as lists, dbpath as a string
+def fix_and_insert_sfl(data, header, dbpath, cruise):
+    #if len(data) != len(header):
+    #    raise IndexError("Different number of items in data and header: h - " + str(len(header)) + ", d - " + str(len(data)))
+
+    dbcolumn_to_fixed_data = {}
+    
+    for d, h in zip(data, header):
+        h = h.upper()
+        h = h.strip('\n')
+        d = d.strip('\n')
+        if h in FLOATS:
+            h = h.strip().replace(' ', '_')
+            try:
+                dbcolumn_to_fixed_data[h] = float(d)
+            except:
+                dbcolumn_to_fixed_data[h] = None
+        elif h in INTS:
+            h = h.strip().replace(' ', '_')
+            try:
+                dbcolumn_to_fixed_data[h] = int(d)
+            except:
+                dbcolumn_to_fixed_data[h] = None
+        elif h in STRS:
+            h = h.strip().replace(' ', '_')
+            try:
+                dbcolumn_to_fixed_data[h] = d
+            except:
+                dbcolumn_to_fixed_data[h] = None
+        # else, do nothing
+
+    # add cruise and data 
+    dbcolumn_to_fixed_data[CRUISE] = cruise
+    if re.match('\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}\+00-?00', dbcolumn_to_fixed_data[FILE]):
+        # New style EVT file names, e.g. 2014-05-15T17-07-08+0000 or 2014-05-15T17-07-08+00-00
+        # Convert to a ISO 8601 date string
+        try :
+          iso_split = dbcolumn_to_fixed_data[FILE].split('T')
+          iso_split[1] = iso_split[1].replace('-', ':')
+          #iso_split[1] = iso_split[1][:-2] + ':' + iso_split[1][-2:]
+          dbcolumn_to_fixed_data[DATE] = 'T'.join(iso_split)
+        except:
+          dbcolumn_to_fixed_data[DATE] = None
+        
+    # any fields that weren't passed in should be None
+    for c in DB_COLUMNS:
+        if not c in dbcolumn_to_fixed_data:
+            dbcolumn_to_fixed_data[c] = None
+
+    # populate list
+    db_tuple = []
+    for c in DB_COLUMNS :
+      db_tuple.append(dbcolumn_to_fixed_data[c])
+
+    # insert into sqlite
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    c.execute("insert into sfl values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", tuple(db_tuple))
+    conn.commit()
+    conn.close()
+
+    #return db_tuple
+
+def insert_file_bulk(sfl_file, db, cruise) :
+    lines = open(sfl_file).readlines()
+    header = lines[0].split('\t')
+    dbpath = os.path.expanduser(db)
+    for line in lines[1:] :
+        data = line.split('\t')
+        conn = sqlite3.connect(dbpath)
+        c = conn.cursor()
+        c.execute("delete from sfl where file == '" + data[0] + "'")
+        conn.commit()
+        conn.close()
+
+        fix_and_insert_sfl(data, header, dbpath, cruise)
+
+def insert_last_entry(db, evt_path, cruise) :
+    dbpath = os.path.expanduser(db)
+    evt_path = os.path.expanduser(evt_path)
+    latest_day = sorted([ name for name in os.listdir(evt_path) if os.path.isdir(os.path.join(evt_path, name)) ])[-1]
+    sfl_file = glob.glob(os.path.join(evt_path,latest_day) + '/*.sfl')[0]
+    lines = open(sfl_file).readlines()
+
+    fix_and_insert_sfl(lines[-1].split('\t'), lines[0].split('\t'), dbpath, cruise)
+
+def insert_last_file(db, evt_path, cruise) :
+    dbpath = os.path.expanduser(db)
+    evt_path = os.path.expanduser(evt_path)
+    latest_day = sorted([ name for name in os.listdir(evt_path) if os.path.isdir(os.path.join(evt_path, name)) ])[-1]
+    sfl_file = glob.glob(os.path.join(evt_path,latest_day) + '/*.sfl')[0]
+    insert_file_bulk(sfl_file, dbpath, cruise)
+
+def insert_from_command_line(db, cruise) :
+    dbpath = os.path.expanduser(db)
+    header = None
+    for line in sys.stdin :
+        if not header:
+            header = line.split('\t')
+        else:
+            fields = line.split('\t')
+            fix_and_insert_sfl(line.split('\t'), header, dbpath, cruise)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description='Insert SFL file data into a popcycle sqlite3 database',
+        prog='fix_sfl.py',
+        formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-d', '--db',
+        help='sqlite3 database file, e.g. ~/popcycle/sqlite/popcycle.db')
+    parser.add_argument(
+        '-c', '--cruise',
+        help='cruise name, e.g. CMOP_3')
+    parser.add_argument(
+        '-e', '--evt_dir',
+        help='EVT data directory, e.g ~/SeaFlow/datafiles/evt/')
+    parser.add_argument(
+        '-s', '--sfl',
+        help='''SFL file.  If not provided, the SFL file for the latest day in
+                EVT_DIR will be used.''')
+
+    args = parser.parse_args()
+
+    if not (args.db and args.cruise and args.evt_dir):
+        print("A required argument is missing\n")
+        parser.print_help()
+    elif not args.sfl:
+        # SFL file for last day
+        insert_last_file(args.db, args.evt_dir, args.cruise)
+    else:
+        # User specified SFL file
+        insert_file_bulk(args.sfl, args.db, args.cruise)
--- executable_scripts/sds_to_sfl.py
+++ executable_scripts/sds_to_sfl.py
@@ -1,7 +1,34 @@
 import sys
-sds_file = sys.argv[1]
+"""
+Convert old Seaflow SDS file format to SFL, with STREAM PRESSURE converted
+to FLOW RATE with user supplied ratio.
+"""
+
+parser = ArgumentParser(
+    description="""Convert old Seaflow SDS file format to SFL, with STREAM
+                PRESSURE converted to FLOW RATE with user supplied ratio.""",
+    prog='sds_to_sfl.py',
+    formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    '--sds',
+    help='Input SDS file')
+parser.add_argument(
+    '--sfl',
+    help='Output SFL file.')
+parser.add_argument(
+    '--instrument_serial_number',
+    help='Seaflow instrument serial number'
+)
+
+args = parser.parse_args()
+if not (args.sds and args.sfl and args.instrument_serial_number):
+    print("A required argument is missing\n")
+    parser.print_help()
+
+ratio_evt_stream = 0.14756
+sds_file = args.sds
 f = open(sds_file)
-f2 = open(sys.argv[2], 'w')
+f2 = open(args.sfl, 'w')
 
 header = f.readline()
 elements = header.split('\t')
@@ -15,21 +42,36 @@ elements[-3] = 'DATE'           # replace 'time' column
 # these periods should be converted to a single space
 elements = [x.replace('.', ' ') for x in elements]
 
+# Find STREAM PRESSURE column index
+stream_index = None
+for i, name in enumerate(elements):
+    if name == 'STREAM PRESSURE':
+        stream_index = i
+if stream_index is None:
+    sys.stderr.write('STREAM PRESSURE not found in SDS file\n')
+    sys.exit(1)
+
+elements.append('FLOW RATE')
+
 f2.write('\t'.join(elements))
 f2.write('\n')
 
 files_seen = set()
 for line in f :
-  elements = line.split('\t')
-  # Build 'FILE' value from 'file' and 'day'
-  elements[-2] = elements[-2] + '/' + elements[-1].strip() + '.evt'
-  # FILE DURATION values fixed at 180 seconds
-  elements[-1] = '180'
-  # Construct a ISO 8601 date string from 'time' field of SDS file and
-  # replace 'time' field with this string
-  elements[-3] = elements[-3].replace(' ', 'T') + '+0000'
+    elements = line.split('\t')
+    # Build 'FILE' value from 'file' and 'day'
+    elements[-2] = elements[-2] + '/' + elements[-1].strip() + '.evt'
+    # FILE DURATION values fixed at 180 seconds
+    elements[-1] = '180'
+    # Construct a ISO 8601 date string from 'time' field of SDS file and
+    # replace 'time' field with this string
+    elements[-3] = elements[-3].replace(' ', 'T') + '+0000'
+    # Create a new FLOW RATE element from STREAM PRESSURE and ratio_evt_stream
+    stream_pressure = float(elements[stream_index])
+    flow_rate = str(1000 * (-9*10**-5 * stream_pressure**4 + 0.0066 * stream_pressure**3 - 0.173 * stream_pressure**2 + 2.5013 * stream_pressure + 2.1059) * ratio_evt_stream)
 
-  if not (elements[-2] in files_seen):
-    files_seen.add(elements[-2])
-    f2.write('\t'.join(elements))
-    f2.write('\n')
+    if not (elements[-2] in files_seen):
+        files_seen.add(elements[-2])
+        elements.append(flow_rate)
+        f2.write('\t'.join(elements))
+        f2.write('\n')
