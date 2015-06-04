487bc9ddbbca0e35d3aa7b37e316e10847518f40
Chris Berthiaume <chrisbee@uw.edu>
2015-06-04 13:47:41 -0700

    Update to tests for new version of testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,3 @@
 library(testthat)
-results <- test_dir("tests/testthat")
-if (any(results$failed > 0)) {
-    status <- 1
-} else {
-    status <- 0
-}
-quit(save="no", status=status)
+results <- test_dir("tests/testthat", reporter="stop")
+print(results)
