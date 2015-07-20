cc69a74fc4752769aa23429d500471fc692d0e6e
Francois Ribalet <ribalet@uw.edu>
2015-07-20 11:33:42 -0700

    Update RT_analysis.sh

--- computer/RT_analysis.sh
+++ computer/RT_analysis.sh
@@ -4,7 +4,7 @@ lckfile=/tmp/SeaFlow_RT.lock
 if shlock -f ${lckfile} -p $$
 then
 echo "cron job starting at $(date)"
-Rscript ~/popcycle-master/executable_scripts/copy_file.R >> ~/cron_job.out 2>&1
+Rscript ~/popcycle-master/executable_scripts/copy_file.R > ~/cron_job.out 2>&1
 python ~/popcycle-master/executable_scripts/fix_sfl_newlines.py >> ~/cron_job.out 2>&1
 python ~/popcycle-master/executable_scripts/import_sfl.py -d ~/popcycle/sqlite/popcycle.db -c realtime -e ~/SeaFlow/datafiles/evt/ >> ~/cron_job.out 2>&1
 Rscript ~/popcycle-master/executable_scripts/cron_job.R >> ~/cron_job.out 2>&1
