f1083b2210dc26b1539b620cba7a74bed76021e5
Chris Berthiaume <chrisbee@uw.edu>
2016-03-21 18:06:44 -0700

    Fix travis tests.

--- setup.R
+++ setup.R
@@ -2,5 +2,3 @@ install.packages("devtools", repos='http://cran.us.r-project.org')
 library(devtools)
 install(dependencies=c("Depends", "Imports", "Suggests"),
         repos='http://cran.us.r-project.org')
-library(popcycle)
-devtools::test()
