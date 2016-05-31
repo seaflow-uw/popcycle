8578275f968a92f65d3270202cf1f1e7ef10adf5
Francois Ribalet <ribalet@uw.edu>
2016-05-31 14:25:08 -0700

    new pictures for wiki

--- R/filter.R
+++ R/filter.R
@@ -69,13 +69,13 @@ filter.notch <- function(evt, origin=NA, width=0.5, notch1=NA, notch2=NA, offset
   if (is.na(notch1)) {
     d.min1 <- min(aligned[which(aligned$fsc_small == max(aligned$fsc_small)),"D1"])
     fsc.max1 <- max(aligned[which(aligned$D1 == d.min1),"fsc_small"])
-    notch1 <- fsc.max1 / (d.min1+ 10000)
+    notch1 <- fsc.max1 / (d.min1)
   }
 
   if (is.na(notch2)) {
     d.min2 <- min(aligned[which(aligned$fsc_small == max(aligned$fsc_small)),"D2"])
     fsc.max2 <- max(aligned[which(aligned$D2 == d.min2),"fsc_small"])
-    notch2 <- fsc.max2 / (d.min2 + 10000)
+    notch2 <- fsc.max2 / (d.min2)
   }
 
   # Filtering focused particles (fsc_small > D + notch)
@@ -85,6 +85,144 @@ filter.notch <- function(evt, origin=NA, width=0.5, notch1=NA, notch2=NA, offset
                 "origin"=origin, "width"=width)
   return(list("opp"=opp, "params"=params))
 }
+
+
+
+
+#' Plot helpful cytograms for exploring filtering parameters.
+#'
+#' @param evt EVT data frame.
+#' @param origin,width,notch1,notch2,offset Filtering parameters. origin,
+#'   notch1, and notch2 will be calculated if NA.
+#' @return None
+#' @export
+plot.filter.cytogram <- function(evt, origin=NA, width=0.5, notch1=NA, notch2=NA, offset=0) {
+  origin <- as.numeric(origin)
+  width <- as.numeric(width)
+
+  notch1 <- as.numeric(notch1)
+  notch2 <- as.numeric(notch2)
+  offset <- as.numeric(offset)
+
+  # linearize the LOG transformed data
+  id <- which(colnames(evt) == "fsc_small" | colnames(evt) == "chl_small" | colnames(evt) =="pe" | colnames(evt) =="fsc_perp" | colnames(evt) =="D1" | colnames(evt) =="D2")
+  if (!any(max(evt[,c(id)]) > 10^3.5)) {
+    evt[,c(id)] <- (log10(evt[,c(id)])/3.5)*2^16
+  }
+
+  # Correction for the difference of sensitivity between D1 and D2
+  if (is.na(origin)) origin <- median(evt$D2-evt$D1)
+
+ # Filtering particles detected by fsc_small
+  evt. <- subset(evt, fsc_small > 1)
+
+  # Fltering aligned particles (D1 = D2), with Correction for the difference of sensitivity between D1 and D2
+  aligned <- subset(evt., D2 < (D1+origin) + width * 10^4 & (D1+origin) < D2 + width * 10^4)
+
+ # finding the notch
+  if (is.na(notch1)) {
+    d.min1 <- min(aligned[which(aligned$fsc_small == max(aligned$fsc_small)),"D1"])
+    fsc.max1 <- max(aligned[which(aligned$D1 == d.min1),"fsc_small"])
+    notch1 <- fsc.max1 / (d.min1)
+  }
+
+  if (is.na(notch2)) {
+    d.min2 <- min(aligned[which(aligned$fsc_small == max(aligned$fsc_small)),"D2"])
+    fsc.max2 <- max(aligned[which(aligned$D2 == d.min2),"fsc_small"])
+    notch2 <- fsc.max2 / (d.min2)
+  }
+
+  # Filtering focused particles (fsc_small > D + notch)
+  opp <- subset(aligned, fsc_small > D1*notch1 - offset*10^4 & fsc_small > D2*notch2 - offset*10^4)
+
+  ################
+  ### PLOTTING ###
+  ################
+  cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
+  percent.opp <- round(100*nrow(opp)/nrow(evt),2)
+
+  origin1 <- origin + width*10^4
+  origin2 <- origin - width*10^4
+
+  if(nrow(evt.) > 10000)  evt. <- evt.[round(seq(1,nrow(evt.), length.out=10000)),]
+  if(nrow(aligned) > 10000)  aligned <- aligned[round(seq(1,nrow(aligned), length.out=10000)),]
+
+  def.par <- par(no.readonly = TRUE) # save default, for resetting...
+
+  par(mfrow=c(2,3),pty='s')
+
+  plot.cytogram(evt., "D1", "D2")
+  mtext("Alignment", side=3, line=4, font=2, col=2)
+  abline(b=1, a=origin1, col='red',lwd=2)
+  abline(b=1, a=origin2, col='red',lwd=2)
+  mtext(paste("D2 - D1=", round(origin,2)),side=3, line=2,font=2)
+  mtext(paste("Width=", width),side=3, line=1,font=2)
+
+  plot.cytogram(aligned, "fsc_small", "D1")
+  mtext("Focus", side=3, line=4, font=2,col=2)
+  mtext(paste("Notch 1=", round(notch1, 2)),side=3, line=2,font=2)
+  mtext(paste("Offset=", offset),side=3, line=1,font=2)
+  abline(b=1/notch1, a=offset*10^4, col=2,lwd=2)
+
+  plot.cytogram(aligned, "fsc_small", "D2")
+  mtext("Focus", side=3, line=4, font=2,col=2)
+  mtext(paste("Notch 2=", round(notch2, 2)),side=3, line=2,font=2)
+  mtext(paste("Offset=", offset),side=3, line=1,font=2)
+  abline(b=1/notch2, a=offset*10^4, col=2,lwd=2)
+
+  plot.cytogram(opp, "fsc_small", "pe")
+  mtext("OPP", side=3, line=1, font=2)
+  plot.cytogram(opp, "fsc_small","chl_small")
+  mtext("OPP", side=3, line=1, font=2)
+  mtext(paste("OPP =", percent.opp,"% EVT"), outer=T,side=1, line=-1.5,font=2,col=2)
+  plot.cytogram(opp, "chl_small","pe")
+  mtext("OPP", side=3, line=1, font=2)
+
+  par(def.par)
+}
+
+#' Plot helpful cytograms for exploring filtering parameters for one file.
+#'
+#' @param evt.dir EVT file directory.
+#' @param file.name File name with julian day directory.
+#' @param origin,width,notch1,notch2,offset Filtering parameters. origin,
+#'   notch1, and notch2 will be calculated if NA.
+#' @return None
+#' @export
+plot.filter.cytogram.by.file <- function(evt.dir, file.name, origin=NA, width=0.5, notch1=NA, notch2=NA, offset=0) {
+  file.name <- clean.file.path(file.name)
+  evt <- readSeaflow(file.path(evt.dir, file.name))
+  plot.filter.cytogram(evt, origin=origin, notch1=notch1, notch2=notch2,
+		                   width=width, offset=offset)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #' find.filter.notch
 #'
@@ -122,13 +260,13 @@ DF <- NULL
         if(is.na(notch1)){
           d.min1 <- min(aligned[which(aligned$fsc_small == max(aligned$fsc_small)),"D1"])
           fsc.max1 <- max(aligned[which(aligned$D1 == d.min1),"fsc_small"])
-          notch.1 <- fsc.max1 / (d.min1+ 10000)
+          notch.1 <- fsc.max1 / (d.min1)
             }
 
         if(is.na(notch2)){
           d.min2 <- min(aligned[which(aligned$fsc_small == max(aligned$fsc_small)),"D2"])
           fsc.max2 <- max(aligned[which(aligned$D2 == d.min2),"fsc_small"])
-          notch.2 <- fsc.max2 / (d.min2 + 10000)
+          notch.2 <- fsc.max2 / (d.min2)
             }
 
         opp <- subset(aligned, fsc_small > D1*notch.1 - offset*10^4 & fsc_small > D2*notch.2 - offset*10^4)
@@ -165,6 +303,7 @@ DF <- NULL
 
   return(DF)
 }
+
 
 #' Filter a list of EVT files.
 #'
