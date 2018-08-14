72cce0605e5f3bf8dfe302c7e6a806ab6d548078
Francois Ribalet <ribalet@uw.edu>
2018-08-14 15:46:48 -0700

    update carbon conversion

--- R/visualization.R
+++ R/visualization.R
@@ -64,14 +64,63 @@ plot.vct.cytogram <- function(opp, para.x = 'fsc_small', para.y = 'chl_small',..
     ## TODO[francois] Order OPP by frequency (most abundant pop plotted first, least abundant pop plotted last)
     id <- which(colnames(opp) == "fsc_small" | colnames(opp) == "chl_small" | colnames(opp) =="pe" | colnames(opp) =="fsc_perp")
     if(max(opp[,c(id)]) > 10^3.5) plot(opp[,c(para.x, para.y)], pch=16, cex=0.3, col = as.numeric(as.factor(opp$pop)), xlim=c(0,2^16), ylim=c(0,2^16),...)
-    else plot(opp[,c(para.x, para.y)], pch=16, cex=0.3, col = as.numeric(as.factor(opp$pop)), log='xy',xlim=c(1,10^3.5), ylim=c(1,10^3.5),...)
-		legend('topleft',legend=(unique(opp$pop)), col=unique(as.numeric(as.factor(opp$pop))), pch=16,pt.cex=0.6,bty='n')
-	} else {
+    else plot(opp[,c(para.x, para.y)], pch=16, cex=0.3, col = as.numeric(as.factor(opp$pop), log='xy', xlim=c(1,10^3.5), ylim=c(1,10^3.5),...)
+       legend('topleft', legend=(unique(opp$pop)), col=unique(as.numeric(as.factor(opp$pop))), pch=16, pt.cex=0.6, bty='n')
+    } else {
 		print("No Gating parameters yet")
 		plot.cytogram(opp, para.x, para.y)
-		mtext(paste("No Gating parameters yet!"),3,line=-1,font=2)
+		mtext(paste("No Gating parameters yet!"), 3, line=-1, font=2)
 	}
 }
+
+
+# #' Plot cytogram with particles colored by population.
+# #'
+# #' @param opp OPP data frame.
+# #' @param para.x Channel to use as x axis.
+# #' @param para.y Channel to use as y axis.
+# #' @param ... Additional parameters for plot()
+# #' @return None
+# #' @export
+# plot.vct.cytogram.deluxe <- function(opp, para.x = 'fsc_small', para.y = 'chl_small',...){
+#
+#   breaks <- 50
+#   def.par <- par(no.readonly = TRUE) # save default, for resetting...
+#   nf <- layout(matrix(c(2,0,1,3),2,2,byrow=TRUE), c(3,1,3,1,3), c(1,3,1,3,1,3), TRUE)
+#   if (!is.null(opp$pop)) {
+# 		par(pty='m')
+#     id <- which(colnames(opp) == "fsc_small" | colnames(opp) == "chl_small" | colnames(opp) =="pe" | colnames(opp) =="fsc_perp")
+#     if(max(opp[,c(id)]) > 10^3.5){
+#       par(mar=c(6,6,1,1))
+#       plot(opp[,c(para.x, para.y)], pch=16, cex=0.3, col = as.numeric(as.factor(opp$pop)), xlim=c(0,2^16), ylim=c(0,2^16))
+#       legend('topleft', legend=(unique(opp$pop)), col=unique(as.numeric(as.factor(opp$pop))), pch=16, pt.cex=0.6, bty='n')
+#       par(mar=c(0,6,1,1))
+#       barplot(hist(opp[,para.x], plot=FALSE,breaks=seq(0,2^16, by=2^16/breaks))$counts, axes=FALSE,axisnames=FALSE, space=0, col=NA)
+#         for(p in unique(opp$pop)) barplot(hist(opp[which(opp$pop == p),para.x], breaks=seq(0,2^16, by=2^16/breaks), plot=FALSE)$counts, add=T, axes=FALSE,axisnames=FALSE, space=0, col=as.numeric(as.factor(opp[which(opp$pop == p),"pop"])))
+#       par(mar=c(6,0,1,1))
+#       barplot(hist(opp[,para.y], breaks=seq(0,2^16, by=2^16/breaks), plot=FALSE)$counts, axes=FALSE,axisnames=FALSE, space=0, col=NA, horiz=T)
+#         for(p in unique(opp$pop)) barplot(hist(opp[which(opp$pop == p),para.y], breaks=seq(0,2^16, by=2^16/50), plot=FALSE)$counts, add=T, axes=FALSE,axisnames=FALSE, space=0, col=as.numeric(as.factor(opp[which(opp$pop == p),"pop"])), horiz=T)
+#
+#       } else{
+#       par(mar=c(6,6,1,1))
+#       plot(opp[,c(para.x, para.y)], pch=16, cex=0.3, col = as.numeric(as.factor(opp$pop)), log='xy', xlim=c(1,10^3.5), ylim=c(1,10^3.5))
+#       legend('topleft', legend=(unique(opp$pop)), col=unique(as.numeric(as.factor(opp$pop))), pch=16, pt.cex=0.6, bty='n')
+#       par(mar=c(0,6,1,1))
+#       barplot(hist(log10(opp[,para.x]), plot=FALSE, breaks=log10(seq(0,10^3.5, by=10^3.5/breaks)))$counts, axes=FALSE,axisnames=FALSE, space=0, col=NA)
+#         for(p in unique(opp$pop)) barplot(hist(log10(opp[which(opp$pop == p),para.x]), plot=FALSE,breaks=log10(seq(0,10^3.5, by=10^3.5/breaks)))$counts, add=T, axes=FALSE,axisnames=FALSE, space=0, col=as.numeric(as.factor(opp[which(opp$pop == p),"pop"])))
+#       par(mar=c(6,0,1,1))
+#       barplot(hist(log10(opp[,para.y]), plot=FALSE,breaks=log10(seq(0,10^3.5, by=10^3.5/breaks)))$counts, axes=FALSE,axisnames=FALSE, space=0, col=NA, horiz=T)
+#         for(p in unique(opp$pop)) barplot(hist(log10(opp[which(opp$pop == p),para.y]), plot=FALSE,breaks=log10(seq(0,10^3.5, by=10^3.5/breaks)))$counts, add=T, axes=FALSE,axisnames=FALSE, space=0, col=as.numeric(as.factor(opp[which(opp$pop == p),"pop"])), horiz=T)
+#
+#     } else {
+# 		print("No Gating parameters yet")
+# 		plot.cytogram(opp, para.x, para.y)
+# 		mtext(paste("No Gating parameters yet!"), 3, line=-1, font=2)
+# 	}
+#   par(def.par)
+# }
+#
+# hist(opp[,para.x], plot=FALSE, breaks=seq(0,10^3.5, by=10^3.5/breaks))
 
 #' Plot cytogram with particles colored by population for one file.
 #'
