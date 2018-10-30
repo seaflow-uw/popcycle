4de56a2378b4322b3c18348d0a397b1dae57525e
Francois Ribalet <ribalet@uw.edu>
2018-10-30 15:40:58 -0700

    update plot function to incorporate Diam + Qc

--- R/visualization.R
+++ R/visualization.R
@@ -3,17 +3,19 @@
 #' @param evtopp EVT or OPP data frame.
 #' @param para.x Channel to use as x axis.
 #' @param para.y Channel to use as y axis.
+#' @param log Log transformation for both x- and y-axis'
 #' @param ... Additional parameters for densCols()
 #' @return None
 #' @usage plot.cytogram(evtopp, para.x = 'fsc_small', para.y = 'chl_small', ...)
 #' @export plot.cytogram
-plot.cytogram <- function(evtopp, para.x = 'fsc_small', para.y = 'chl_small', ...) {
+plot.cytogram <- function(evtopp, para.x = 'fsc_small', para.y = 'chl_small', log=T, ...) {
   cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
 
   par(pty='s')
   id <- which(colnames(evtopp) == "fsc_small" | colnames(evtopp) == "chl_small" | colnames(evtopp) =="pe" | colnames(evtopp) =="fsc_perp")
-  if(max(evtopp[,c(id)]) > 10^3.5) plot(evtopp[,c(para.x, para.y)], pch=16, cex=0.3, col = densCols(evtopp[,c(para.x, para.y)], colramp = cols), ...)
-  else plot(evtopp[,c(para.x, para.y)], pch=16, cex=0.3, col = densCols(log10(evtopp[,c(para.x, para.y)]), colramp = cols), log='xy', ...)
+  if(log) plot(evtopp[,c(para.x, para.y)], pch=16, cex=0.3, col = densCols(log10(evtopp[,c(para.x, para.y)]), colramp = cols), log='xy', ...)
+  else plot(evtopp[,c(para.x, para.y)], pch=16, cex=0.3, col = densCols(evtopp[,c(para.x, para.y)], colramp = cols), ...)
+
 }
 
 
@@ -23,11 +25,12 @@ plot.cytogram <- function(evtopp, para.x = 'fsc_small', para.y = 'chl_small', ..
 #' @param para.x Channel to use as x axis.
 #' @param para.y Channel to use as y axis.
 #' @param min.count Minimum number of particles for drawing contour lines.
+#' @param log Log transformation for both x- and y-axis'
 #' @param ... Additional parameters for plot()
 #' @return None
 #' @usage plot.vct.cytogram(opp, para.x = 'fsc_small', para.y = 'chl_small', min.count=300, ...)
 #' @export plot.vct.cytogram
-plot.vct.cytogram <- function(opp, para.x = 'fsc_small', para.y = 'chl_small', min.count=300, ...) {
+plot.vct.cytogram <- function(opp, para.x = 'fsc_small', para.y = 'chl_small', min.count=300, log=T, ...) {
   require(scales, quietly=T)
   require(flowViz, quietly=T)
 
@@ -37,26 +40,40 @@ plot.vct.cytogram <- function(opp, para.x = 'fsc_small', para.y = 'chl_small', m
     for(i in 1:length(popu)) opp[which(opp$pop==popu[i]),'col'] <- color[i]
 
 		par(pty='s')
-        d <- log(mean(par("pin")))
-        plot(opp[,c(para.x, para.y)], cex=d, pch=16, col = alpha(opp$col,0.4), log='xy', xlim=c(1,10^3.5), ylim=c(1,10^3.5),...)
+    d <- log(mean(par("pin")))
+
+        if(log){
+          plot(opp[,c(para.x, para.y)], cex=d, pch=16, col = alpha(opp$col,0.4), log='xy', ...)
           par(new=T)
-          plot(1,1,pch=NA, xaxt='n', yaxt='n', xlim=log10(c(1,10^3.5)), ylim=log10(c(1,10^3.5)), xlab=NA, ylab=NA, bty='n')
+          plot(1,1,pch=NA, xaxt='n', yaxt='n', xlim=log10(range(opp[,c(para.x)])), ylim=log10(range(opp[,c(para.y)])), xlab=NA, ylab=NA, bty='n')
 
+          for(i in c('prochloro','synecho')){
+              df <- subset(opp, pop==i)
+                if(nrow(df) < min.count) next
+              c <- rgb2hsv(col2rgb(df$col))
+              cols <- rainbow(10,s=c[2], v=c[3],start=c[1], alpha=0.1)
+              df.f <- flowFrame(as.matrix(log10(df[,c(para.x, para.y)])))
+              contour(df.f,fill=alpha('white',0.5), col='grey', grid.size=c(30,30),  add=T)
+              contour(df.f,fill=cols, col=cols, grid.size=c(30,30), add=T)
+            }
 
-        for(i in c('prochloro','synecho')){
-          df <- subset(opp, pop==i)
-            if(nrow(df) < min.count) next
-          c <- rgb2hsv(col2rgb(df$col))
-          cols <- rainbow(10,s=c[2], v=c[3],start=c[1], alpha=0.1)
-          df.f <- flowFrame(as.matrix(log10(df[,c(para.x, para.y)])))
-          contour(df.f,fill=alpha('white',0.5), col='grey', grid.size=c(30,30),  add=T)
-          contour(df.f,fill=cols, col=cols, grid.size=c(30,30), add=T)
-        }
+          }else{
+            plot(opp[,c(para.x, para.y)], cex=d, pch=16, col = alpha(opp$col,0.4), ...)
 
-    legend('topleft', legend=(unique(opp$pop)), col=unique(opp$col), pch=16, pt.cex=0.6, bty='n')
+            for(i in c('prochloro','synecho')){
+                df <- subset(opp, pop==i)
+                  if(nrow(df) < min.count) next
+                c <- rgb2hsv(col2rgb(df$col))
+                cols <- rainbow(10,s=c[2], v=c[3],start=c[1], alpha=0.1)
+                df.f <- flowFrame(as.matrix(df[,c(para.x, para.y)]))
+                contour(df.f,fill=alpha('white',0.5), col='grey', grid.size=c(30,30),  add=T)
+                contour(df.f,fill=cols, col=cols, grid.size=c(30,30), add=T)
+                }
+              }
+          legend('topleft', legend=(unique(opp$pop)), col=unique(opp$col), pch=16, pt.cex=0.6, bty='n')
     } else {
 		print("No Gating parameters yet")
-		plot.cytogram(opp, para.x, para.y, ...)
+		plot.cytogram(opp, para.x, para.y, log=log, ...)
 		mtext(paste("No Gating parameters yet!"), 3, line=-1, font=2)
 	}
 }
@@ -68,11 +85,12 @@ plot.vct.cytogram <- function(opp, para.x = 'fsc_small', para.y = 'chl_small', m
 #' @param poly.log Gating polygon(s) to draw (optional)
 #' @param para.x Channel to use as x axis.
 #' @param para.y Channel to use as y axis.
+#' @param log Log transformation for both x- and y-axis'
 #' @return None
 #' @usage plot.gating.cytogram(opp, gating.log=NULL, para.x = 'fsc_small', para.y = 'chl_small')
 #' @export plot.gating.cytogram
-plot.gating.cytogram <- function(opp, gating.log=NULL, para.x = 'fsc_small', para.y = 'chl_small') {
-	plot.cytogram(opp, para.x, para.y)
+plot.gating.cytogram <- function(opp, gating.log=NULL, para.x = 'fsc_small', para.y = 'chl_small', log=T,...) {
+	plot.cytogram(opp, para.x, para.y, log=log)
 	if (!is.null(gating.log)) {
 		for (i in 1:length(gating.log)) {
       method <- gating.log[[i]]$method
@@ -142,11 +160,16 @@ plot.map <- function(stat, popname, param, ...){
 #' @export plot.time
 plot.time <- function(stat, popname, param, ...){
   require(plotrix, quietly=T)
+  popu <- c('unknown','picoeuk','croco','prochloro','synecho','beads')
+  color <- c('grey','seagreen3','darkorchid','skyblue3','orange','red3')
+  for(i in 1:length(popu)) stat[which(stat$pop==popu[i]),'col'] <- color[i]
 
   stat$time <- as.POSIXct(stat$time,format="%FT%T",tz='GMT')
-  pop <- subset(stat, pop == popname)
-  if(any(colnames(stat)== paste0(param,".sd"))) plotCI(pop$time, pop[,param], uiw=pop[,paste0(param,".sd")], sfrac=0,xlab="Time", ylab=paste(param),main=paste(popname), scol='grey',...)
-  else plot(pop$time, pop[,param], xlab="Time", ylab=paste(param),main=paste(popname),...)
+  pop <- subset(stat, pop == popname & quantile == 50)
+  pop_lwr <- subset(stat, pop == popname & quantile == 2.5)
+  pop_upr <- subset(stat, pop == popname & quantile == 97.5)
 
+  plot(pop$time, pop[,param], xlab="Time", ylab=paste(param),main=paste(popname),pch=21, bg=alpha(pop$col,0.5), ...)
+  arrows(pop_lwr$time, pop_lwr[,param], pop_upr$time, pop_upr[,param],  code = 3, length=0, col='grey', lwd=2)
 
 }
