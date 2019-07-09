874e77de87e27a51c8603ea7789f671bab6409ed
Chris Berthiaume <chrisbee@uw.edu>
2019-07-09 16:04:27 -0700

    Improve docker build process

--- setup.R
+++ setup.R
@@ -2,18 +2,5 @@
 # To install to a non-default library location, set the environment variable
 # R_LIBS_USER.
 
-# Install and attach devtools
-if (!requireNamespace("devtools", quietly = TRUE)) {
-  install.packages("devtools", repos='http://cran.us.r-project.org')
-}
-
-# Install and attach BiocManager
-if (!requireNamespace("BiocManager", quietly = TRUE)) {
-  install.packages("BiocManager", repos='http://cran.us.r-project.org')
-}
-if (!requireNamespace("flowDensity", quietly = TRUE)) {
-  BiocManager::install("flowDensity", update=FALSE)
-}
-
-# Install this package and the packages it imports
-devtools::install(dependencies=TRUE, upgrade=FALSE)
+system2("Rscript", c("./install-deps-only.R"))
+system2("Rscript", c("./install-package-only.R"))
