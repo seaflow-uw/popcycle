e9b2a250c6447350a97f133ced538c598ec7c5f3
Chris Berthiaume <chrisbee@uw.edu>
2020-07-21 11:16:01 -0700

    Add Docker files for popcycle image and update setup script

--- setup.R
+++ setup.R
@@ -1,12 +1,14 @@
 #!/usr/bin/env Rscript
 # To install to a non-default library location, set the environment variable
-# R_LIBS_USER. To update dependencies, set environment variable
-# POPCYCLE_UPDATE_DEPS=TRUE or some other string for which as.logical(x) returns
-# TRUE.
-update_flag <- as.logical(Sys.getenv("POPCYCLE_UPDATE_DEPS"))
-if (is.na(update_flag) | is.null(update_flag)) {
-  update_flag <- FALSE
+# R_LIBS_USER. Set Ncpus option to devtools::install with environment
+# variable INSTALL_NCPUS. If not set Ncpus will default to 4.
+
+ncpus <- as.numeric(Sys.getenv("INSTALL_NCPUS"))
+if (is.na(ncpus)) {
+  ncpus <- 4
 }
+
+cat("Installing using Ncpus =", ncpus, "\n")
 
 # Install and attach devtools
 if (!requireNamespace("devtools", quietly=TRUE)) {
@@ -17,9 +19,19 @@ if (!requireNamespace("devtools", quietly=TRUE)) {
 if (!requireNamespace("BiocManager", quietly=TRUE)) {
   install.packages("BiocManager", repos='http://cran.us.r-project.org')
 }
-if (!requireNamespace("flowDensity", quietly=TRUE)) {
-  BiocManager::install("flowDensity", update=update_flag)
-}
 
+# Prevent warnings from package installs from turning into errors that halt
+# installation. This variable is set here because a a minor markup error in a
+# flowDensity Rd file prevented it from installing successfully with
+# devtools::install_deps. The warning was
+# bad markup (extra space?) at plotDens.Rd:62:19
+# flowDensity tarball was
+# https://bioconductor.org/packages/3.11/bioc/src/contrib/flowDensity_1.22.0.tar.gz
+# Web resources that led to this solution
+# https://github.com/r-lib/remotes/issues/403
+# https://github.com/r-lib/remotes#environment-variables
+if (Sys.getenv("R_REMOTES_NO_ERRORS_FROM_WARNINGS") == "") {
+  Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true")
+}
 # Install this package and the packages it imports
-devtools::install(dependencies=TRUE, upgrade=update_flag)
+devtools::install(dependencies=TRUE, upgrade="never", Ncpus=ncpus)
