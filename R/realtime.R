7aa2fd2c15ae8e256b51750fcbdb6910a3850181
Chris Berthiaume <chrisbee@uw.edu>
2021-10-20 18:23:41 -0700

    Add tsdata output for realtime

--- R/realtime.R
+++ R/realtime.R
@@ -33,21 +33,70 @@ create_realtime_meta <- function(db, quantile_) {
     date, lat, lon, conductivity, salinity, ocean_tmp, par, stream_pressure,
     event_rate, volume, all_count, opp_count, evt_count, opp_evt_ratio
   )
-  
+
   return(meta)
+}
+
+#' Write SFL metadata as a TSDATA file
+#'
+#' @param meta SFL metadata dataframe created by create_realtime_meta()
+#' @param project Project identifier
+#' @param outfile Output file path
+#' @param filetype Filetype identifier
+#' @param description Long form description of this file
+#' @export
+write_realtime_meta_tsdata <- function(meta, project, outfile, filetype="SeaFlowSFL", description="SeaFlow SFL data") {
+  fh <- file(outfile, open="wt")
+  writeLines(filetype, fh)
+  writeLines(project, fh)
+  writeLines(description, fh)
+  writeLines(paste("ISO8601 timestamp", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", sep="\t"), fh)
+  writeLines(paste("time", "float", "float", "float", "float", "float", "float", "float", "float", "float", "integer", "integer", "integer", "float", sep="\t"), fh)
+  writeLines(paste("NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", sep="\t"), fh)
+  close(fh)
+  readr::write_delim(meta, outfile, delim="\t", col_names=TRUE, append=TRUE)
 }
 
 #' Create a population data tibble for one quantile from the VCT
 #'
 #' @param db popcycle database file.
 #' @param quantile OPP filtering quantile to use.
+#' @param with_abundance Include volume normalized "abundance" column
 #' @return A tibble of realtime population data
 #' @export
-create_realtime_bio <- function(db, quantile_) {
-  stat <- tibble::as_tibble(stat) %>%
+create_realtime_bio <- function(db, quantile_, with_abundance=FALSE) {
+  bio <- tibble::as_tibble(get.stat.table(db)) %>%
     dplyr::mutate(date=as.POSIXct(time, format="%FT%T", tz="UTC")) %>%
     dplyr::filter(quantile == quantile_) %>%
-    dplyr::select(date, pop, n_count, fsc_med, chl_med, pe_med, diam_mid_med, Qc_mid_med)
-  
-  return(stat)
+    dplyr::select(date, pop, n_count, abundance)
+  if (! with_abundance) {
+    bio <- bio %>% dplyr::select(-c("abundance"))
+  }
+  return(bio)
+}
+
+#' Write population data as a TSDATA file
+#'
+#' @param bio Population dataframe created by create_realtime_bio()
+#' @param project Project identifier
+#' @param outfile Output file path
+#' @param filetype Filetype identifier
+#' @param description Long form description of this file
+#' @export
+write_realtime_bio_tsdata <- function(bio, project, outfile, filetype="SeaFlowPop", description="SeaFlow population data") {
+  fh <- file(outfile, open="wt")
+  writeLines(filetype, fh)
+  writeLines(project, fh)
+  writeLines(description, fh)
+  if ("abundance" %in% colnames(bio)) {
+    writeLines(paste("ISO8601 timestamp", "NA", "NA", "NA", sep="\t"), fh)
+    writeLines(paste("time", "category", "integer", "float", sep="\t"), fh)
+    writeLines(paste("NA", "NA", "NA", "NA", sep="\t"), fh)
+  } else {
+    writeLines(paste("ISO8601 timestamp", "NA", "NA", sep="\t"), fh)
+    writeLines(paste("time", "category", "integer", sep="\t"), fh)
+    writeLines(paste("NA", "NA", "NA",  sep="\t"), fh)
+  }
+  close(fh)
+  readr::write_delim(bio, outfile, delim="\t", col_names=TRUE, append=TRUE)
 }
